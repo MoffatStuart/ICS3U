aa110bb266934d718e0bd783b44915e7337fc8d2
Stuart Moffat <stuartmoffat147@hotmail.com>
2016-03-02 14:45:04 -0500

    First Java program/first push...

---
+++ ICS3U/src/moffat/HelloWorld.java
@@ -0,0 +1,15 @@
+package moffat;
+/*helloWorld.java
+ * This program prints "Hello World" to the program
+ * Stuart Moffat
+ * March 2nd 2016
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		//This prints "Hello world" to the console.
+		System.out.println("Hello World! :)");
+		
+	}
+
+}

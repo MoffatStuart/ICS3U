ac61507b3ff38e32d47962a0d98d167b9b7b4c53
Stuart Moffat <stuartmoffat147@hotmail.com>
2016-03-09 15:03:19 -0500

    Input for Arithmetic assignment

---
+++ ICS3U/src/moffat/ArithmeticAssignment.java
@@ -0,0 +1,41 @@
+package moffat;
+
+import java.util.Scanner;
+
+/*Stuart Moffat
+ * March 9th 2016
+ * 
+ */
+public class ArithmeticAssignment {
+
+	public static void main(String[] args) {
+		
+		Scanner myInput = new Scanner(System.in);
+		
+		double num1;
+		double num2;
+		double num3;
+		double num4;
+		double num5;
+		
+		System.out.println("Please input a number.");
+		num1 = Integer.parseInt(myInput.nextLine());
+		
+		System.out.println("Please input another number.");
+		num2 = Integer.parseInt(myInput.nextLine());
+		
+		System.out.println("Please input another number.");
+		num3 = Integer.parseInt(myInput.nextLine());
+		
+		System.out.println("Please input another number.");
+		num4 = Integer.parseInt(myInput.nextLine());
+		
+		System.out.println("Please input your final number.");
+		num5 = Integer.parseInt(myInput.nextLine());
+		
+		
+		
+
+	}
+
+}
